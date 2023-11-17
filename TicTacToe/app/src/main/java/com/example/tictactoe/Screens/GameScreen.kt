4f0b04372503504cc26b66a1ba96057b8bea076a
ahmed20011992 <ahmetelhasan1201@gmail.com>
2023-11-17 01:27:45 +0100

    Hello my friend this is that i showed you by SMS a making a basic form for the gameScreen

--- TicTacToe/app/src/main/java/com/example/tictactoe/Screens/GameScreen.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/Screens/GameScreen.kt
@@ -1,19 +1,110 @@
 package com.example.tictactoe.Screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.DefaultShadowColor
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import okio.blackholeSink
+import org.w3c.dom.Text
+
+
+
 
 @Composable
-fun GameScreen(navController: NavController = rememberNavController()){
- Column (modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-     horizontalAlignment = Alignment.CenterHorizontally
-     ){
+fun GameScreen(navController: NavController = rememberNavController()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
 
- }
+        ) {
+            Text(text = "Player X", fontSize = 16.sp)
+            Text(text = "Drow", fontSize = 16.sp)
+            Text(text = "Player O", fontSize = 16.sp)
+
+        }
+       Text(
+           text = "Tic Tac Toe",
+           fontSize = 50.sp,
+           fontWeight = FontWeight.Bold,
+           fontFamily = FontFamily.Cursive,
+           color = Color.DarkGray,
+           modifier = Modifier.graphicsLayer {  }
+
+       )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .shadow(
+                    elevation = 10.dp,
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .clip(RoundedCornerShape(20.dp))
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+
+        ){
+
+        }
+        Row (modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+            ){
+            Text(text = "Player  'O' turn",
+                fontSize = 25.sp,
+                fontStyle = FontStyle.Italic
+                )
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(5.dp),
+                elevation = ButtonDefaults.elevatedButtonElevation(),
+                colors = ButtonDefaults.buttonColors(
+
+
+                )
+
+            ) {
+                Text(text = "Playe More")
+
+            }
+
+        }
+    }
 }
