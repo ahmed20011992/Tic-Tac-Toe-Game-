da63255d50f8ff01aa70f1feaa885ee9311a2b90
ahmed20011992 <ahmetelhasan1201@gmail.com>
2023-11-24 01:11:08 +0100

    tt

---
+++ TicTacToe/app/src/main/java/com/example/tictactoe/Viewmodel/GameViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.tictactoe.Viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+import androidx.compose.runtime.mutableStateOf
+
+
+import androidx.compose.runtime.mutableStateOf
+
+
+class SquareValue {
+    // A square on the Tic Tac Toe board can have three states: 'X', 'O', or empty (' ')
+    var value by mutableStateOf(' ')
+}
+
+
+
+
+
+
